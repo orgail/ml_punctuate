38b1392897db13515312c26e7acdd9b2e7edd721
Denis <tetragrom@mail.ru>
2022-11-20 15:03:19 +0400

    add streamlit app

---
+++ streamlit_punctuate.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from sbert_punc_case_ru import SbertPuncCase
+import string
+
+# Создаём объект - модель для преобразования текста
+model = SbertPuncCase()
+
+# Формируем заголовок для браузера
+st.title("Восстанавление пунктуации текста")
+st.subheader("Применяется после аудиораспознавания, а также при сомнениях после перевода текста.")
+
+# Забираем текст, введённый пользователем в браузере
+text_to_punctuate = st.text_area("Вставьте сюда текст на русском языке", value="")
+
+# обработаем текст, на случай, если пользователь ввёл не по правилам -
+# преобразуем в нижний регистр и уберём знаки препинания.
+text_to_punctuate = text_to_punctuate.lower()
+text_to_punctuate = text_to_punctuate.translate(str.maketrans('', '', string.punctuation))
+
+# Преобразуем текст, проводя его через модель
+text_punctuated =  model.punctuate(text_to_punctuate)
+
+# Пишем в браузере преобразованный текст, 
+st.text_area("Преобразованный текст. Его можно выделить и скопировать.", value=text_punctuated, disabled=False)
