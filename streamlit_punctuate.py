5e6184cd8d4be097e879c139dec6f61e0abf9996
Denis <tetragrom@mail.ru>
2023-04-08 19:47:53 +0400

    black streamlit_punctuate.py
    conversion to the PEP8 standard

--- streamlit_punctuate.py
+++ streamlit_punctuate.py
@@ -7,20 +7,24 @@ model = SbertPuncCase()
 
 # Формируем заголовок для браузера
 st.title("Восстанавление пунктуации текста")
-st.subheader("Применяется после аудиораспознавания, а также при сомнениях после перевода текста.")
+st.subheader(
+    "Применяется после аудиораспознавания, а также при сомнениях после перевода текста."
+)
 
 # Забираем текст, введённый пользователем в браузере
-# ! Была изменена переменная text_to_punctuate на input_text 
+# ! Была изменена переменная text_to_punctuate на input_text
 # ! для возможности проверки введённого пользователем текста в целях отладки приложения.
-input_text = st.text_area("Вставьте сюда текст на русском языке, например - однако на улице прекрасная погода",
-                                 value="")
+input_text = st.text_area(
+    "Вставьте сюда текст на русском языке, например - однако на улице прекрасная погода",
+    value="",
+)
 
 # обработаем текст, на случай, если пользователь ввёл не по правилам -
 # преобразуем в нижний регистр и уберём знаки препинания.
-# ! Была изменена переменная text_to_punctuate на lowercase_text 
-# ! Причины изменения можно посмотреть выше. 
+# ! Была изменена переменная text_to_punctuate на lowercase_text
+# ! Причины изменения можно посмотреть выше.
 lowercase_text = input_text.lower()
-text_to_punctuate = lowercase_text.translate(str.maketrans('', '', string.punctuation))
+text_to_punctuate = lowercase_text.translate(str.maketrans("", "", string.punctuation))
 
 
 def text_punctuated(text_to_punctuate):
@@ -29,5 +33,8 @@ def text_punctuated(text_to_punctuate):
 
 
 # Пишем в браузере преобразованный текст,
-st.text_area("Преобразованный текст. Его можно выделить и скопировать.", value=text_punctuated(text_to_punctuate),
-             disabled=False)
+st.text_area(
+    "Преобразованный текст. Его можно выделить и скопировать.",
+    value=text_punctuated(text_to_punctuate),
+    disabled=False,
+)
