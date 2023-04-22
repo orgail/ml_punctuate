c95fb8c24d1953ec32259f4eca931129f1045470
Denis <tetragrom@mail.ru>
2023-04-22 14:39:03 +0400

    Тест работы исключения

--- test_streamlit_punctuate1.py
+++ test_streamlit_punctuate1.py
@@ -1,5 +1,15 @@
 from streamlit_punctuate import text_punctuated
+import pytest
 
 
 def test_punctuated():
     assert text_punctuated("однако на улице прекрасная погода") == "Однако, на улице прекрасная погода."
+
+
+def test_raises():
+    with pytest.raises(Exception) as exc_info:   
+        raise Exception("Невозможно определить язык! Попробуйте \
+            ввести текст еще раз.")
+    # these asserts are identical; you can use either one   
+    assert exc_info.value.args[0] == "Невозможно определить язык! Попробуйте \
+            ввести текст еще раз."
