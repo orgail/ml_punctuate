8545ae91ef6ceb0cfd01d0a8670061f3e4cc2c38
Denis <tetragrom@mail.ru>
2023-04-24 20:07:55 +0400

    Добавил тест языка текста
    Добавил тесты функции определения языка test_lang_detection() - при вводе текста на не русском и нечитаемыми символами

--- streamlit_punctuate.py
+++ streamlit_punctuate.py
@@ -1,30 +1,8 @@
 import streamlit as st
 from sbert_punc_case_ru import SbertPuncCase
+from langdetect import detect
+from langdetect.lang_detect_exception import LangDetectException
 import string
-
-# Создаём объект - модель для преобразования текста
-model = SbertPuncCase()
-
-# Формируем заголовок для браузера
-st.title("Восстанавление пунктуации текста")
-st.subheader(
-    "Применяется после аудиораспознавания, а также при сомнениях после перевода текста."
-)
-
-# Забираем текст, введённый пользователем в браузере
-# ! Была изменена переменная text_to_punctuate на input_text
-# ! для возможности проверки введённого пользователем текста в целях отладки приложения.
-input_text = st.text_area(
-    "Вставьте сюда текст на русском языке, например - однако на улице прекрасная погода",
-    value="",
-)
-
-# обработаем текст, на случай, если пользователь ввёл не по правилам -
-# преобразуем в нижний регистр и уберём знаки препинания.
-# ! Была изменена переменная text_to_punctuate на lowercase_text
-# ! Причины изменения можно посмотреть выше.
-lowercase_text = input_text.lower()
-text_to_punctuate = lowercase_text.translate(str.maketrans("", "", string.punctuation))
 
 
 def text_punctuated(text_to_punctuate):
@@ -32,9 +10,52 @@ def text_punctuated(text_to_punctuate):
     return model.punctuate(text_to_punctuate)
 
 
-# Пишем в браузере преобразованный текст,
-st.text_area(
-    "Преобразованный текст. Его можно выделить и скопировать.",
-    value=text_punctuated(text_to_punctuate),
-    disabled=False,
-)
+# Функция определения языка помощью библиотеки langdetect
+# Пробуем определить, на каком языке написан введенный текст.
+# Если удалось определить, что текст не русский, выдаем сообщение
+# об ошибке.
+# Если удалось определить, что текст русский:
+# обработаем текст, на случай, если пользователь ввёл не по правилам -
+# преобразуем в нижний регистр и уберём знаки препинания.
+# Пишем в браузере преобразованный текст.
+# Если не удалось определить, на каком языке написан текст, выдаем
+# сообщение об ошибке.
+def lang_detection(input_text):
+    try:
+        detected_text = detect(input_text)
+        if detected_text != 'ru':
+            st.error("Язык введенного текста не русский! Попробуйте ввести \
+                текст еще раз.")
+            return "Язык введенного текста не русский! Попробуйте ввести \
+                текст еще раз."
+        else:
+            lowercase_text = input_text.lower()
+            text_to_punctuate = lowercase_text.translate(str.maketrans('', '',
+                                                         string.punctuation))
+            st.text_area("Преобразованный текст. Его можно выделить и \
+                скопировать.",
+                         value=text_punctuated(text_to_punctuate),
+                         disabled=False)
+    except LangDetectException:
+        st.error("Невозможно определить язык! Попробуйте ввести текст еще \
+            раз.")
+        return "Невозможно определить язык! Попробуйте ввести текст еще раз."
+
+
+# Создаём объект - модель для преобразования текста
+model = SbertPuncCase()
+
+# Формируем заголовок для браузера
+st.title("Восстанавление пунктуации текста")
+st.subheader("Применяется после аудиораспознавания, а также при сомнениях \
+    после перевода текста.")
+
+# Забираем текст, введённый пользователем в браузере
+input_text = st.text_area("Вставьте сюда текст на русском языке, например - \
+    однако на улице прекрасная погода", value="")
+
+
+# Проверяем, введен ли какой-нибудь текст.
+# Если текст введен, то выполненяем функцию с определением языка
+if len(input_text) > 0:
+    lang_detection(input_text)
