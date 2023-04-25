921574505bdd88bdc3c646a0641f079b84cc1b53
andy-kuzn <andrey.kuzn.1@gmail.com>
2023-04-25 22:10:14 +0300

    Update test_streamlit_punctuate1.py

--- test_streamlit_punctuate1.py
+++ test_streamlit_punctuate1.py
@@ -1,5 +1,22 @@
+import pytest
+from streamlit_punctuate import check_input
+from langdetect.lang_detect_exception import LangDetectException
 from streamlit_punctuate import text_punctuated
 
 
+def test_input_ru():
+    assert check_input("однако на улице прекрасная погода") == "ru"
+
+
+def test_input_not_ru():
+    assert check_input("This text is in English.") != "ru"
+
+
+def test_input_symbol():
+    with pytest.raises(LangDetectException):
+        check_input("#%0)#_)")
+
+
 def test_punctuated():
-    assert text_punctuated("однако на улице прекрасная погода") == "Однако, на улице прекрасная погода."
+    assert text_punctuated("однако на улице прекрасная погода") \
+        == "Однако, на улице прекрасная погода."
